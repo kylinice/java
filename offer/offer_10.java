fdd5fec69da76c8901d096d28b1acebb46b086b8
kylinice <1069652997@qq.com>
2018-07-16 20:03:46 +0800

    2018-07-16

---
+++ offer/offer_10.java
@@ -0,0 +1,21 @@
+package offer;
+
+public class offer_10 {
+    public static int RectCover(int target) {
+        int s=0,a=1,b=2;
+        if(target<=2){
+            return target;
+        }
+        while(target>=3){
+            s = a + b;
+            a = b;
+            b = s;
+            target--;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RectCover(4));
+    }
+}
---
+++ offer/offer_11.java
@@ -0,0 +1,12 @@
+package offer;
+
+public class offer_11 {
+    public static int NumberOf1(int n) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        NumberOf1(5);
+    }
+}
