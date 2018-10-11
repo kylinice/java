6adb6491afe66d7ec1ef03c85d4a6fb53923665c
kylinice <1069652997@qq.com>
2018-10-11 11:17:40 +0800

    2018-10-11 11:12 modify offer_6

--- offer/offer_6_minNumberInRotateArray.java
+++ offer/offer_6_minNumberInRotateArray.java
@@ -4,10 +4,11 @@ package offer;
 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
  */
 public class offer_6_minNumberInRotateArray {
-    public static int minNumberInRotateArray(int [] array) {
+    public static int findMin(int [] array) {
         int min = 0;
         if(array.length == 0)
             return min;
+        min = array[0];
         for(int i=0;i<array.length-1;i++){
             if(array[i+1]<array[i]){
                 min = array[i+1];
@@ -16,8 +17,33 @@ public class offer_6_minNumberInRotateArray {
         }
         return min;
     }
+    public static int minNumberInRotateArray(int[] array) {
+        if (array.length == 0)
+            return 0;
+        int left = 0;
+        int right = array.length - 1;
+        int middle = left;
+        while (array[left]>=array[right]) {
+            if(right-left==1){
+                middle = right;
+                break;
+            }
+            middle = (left+right)/2;
+            if(array[left]==array[right] && array[middle]==array[left]){
+                return findMin(array);
+            }
+            if (array[middle] >= array[left]) {
+                left = middle;
+            }
+            if (array[middle] <= array[right]) {
+                right = middle;
+            }
+        }
+        return array[middle];
+    }
     public static void main(String[] args) {
-        int a[] = {3,4,5,1,2};
-        System.out.println(minNumberInRotateArray(a));
+        int[] a = {3,4,5,1,2};
+        int[] b = {1,0,1,1,1};
+        System.out.println(minNumberInRotateArray(b));
     }
 }
